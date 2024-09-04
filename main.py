59258c2647fbe254dc3a33d0656f4809399d6c0b
TJ Kolade <teejkols1@gmail.com>
2024-09-04 16:48:53 -0500

    new repo

---
+++ main.py
@@ -0,0 +1,46 @@
+from src.mlProject import logger
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
+STAGE_NAME = 'Data Ingestion stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<\n\n')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data Validation stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<\n\n')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Data Transformation stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_validation = DataTransformationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<\n\n')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Model Trainer stage'
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
+    data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<<\n\n')
+except Exception as e:
+    logger.exception(e)
+    raise e
